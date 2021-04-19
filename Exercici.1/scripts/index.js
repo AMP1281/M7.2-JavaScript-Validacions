f7f57e43170e2f78c657a1cd055ed0c940c27218
Anna Montserrat <amp1281@gmail.com>
2021-04-19 18:09:29 +0200

    modis

--- Exercici.1/scripts/index.js
+++ Exercici.1/scripts/index.js
@@ -1,5 +1,19 @@
 
 //Validar formulari registre
 
-//const formulario3 = document.getElementById("formulario3");
-//const inputs = document.querySelectorAll("#formulario3");
+const formulario3 = document.getElementById("formulario3");
+const inputs = document.querySelectorAll("#formulario3");
+
+const validarFormulario3= () =>{
+    
+}
+
+
+inputs.forEach((input) => {
+    input.addEventListener("keyup", validarFormulario3);
+    input.addEventListener("blur", validarFormulario3);
+});
+
+formulario3.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
