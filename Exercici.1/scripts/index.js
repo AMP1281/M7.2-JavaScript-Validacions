692e95c52d26e9941d553fb64d303e26c20f835e
Anna Montserrat <amp1281@gmail.com>
2021-04-19 17:39:57 +0200

    test

--- Exercici.1/scripts/index.js
+++ Exercici.1/scripts/index.js
@@ -1,5 +1,5 @@
 
 //Validar formulari registre
 
-const formulario = document.formulario;
-const inputs = document.querySelectorAll;
+//const formulario3 = document.getElementById("formulario3");
+//const inputs = document.querySelectorAll("#formulario3");
